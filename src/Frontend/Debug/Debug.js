be92cf8fb2dc8e2058cc8bc881794e6e81f0a731
Johnathon Selstad <makeshifted@gmail.com>
2021-03-25 19:02:03 -0700

    Clean up Debug File a bit

--- src/Frontend/Debug/Debug.js
+++ src/Frontend/Debug/Debug.js
@@ -6,24 +6,25 @@ class Debug {
     /** Reroute Console Errors to the Main Screen (for mobile) 
      * @param {LeapShapeEngine} engine */
     constructor(engine) {
-        this.engine = engine;
+        // Route Worker Errors Here
+        engine.registerCallback("error", this.fakeError.bind(this));
+
+        // Intercept Main Window Errors as well
         window.realConsoleError = console.error;
         window.addEventListener('error', function (event) {
-            console.log(event);
             let errorNode = window.document.createElement("div");
             let path = event.filename.split("/");
             errorNode.innerHTML = (path[path.length-1] + ":"+ event.lineno + " - " + event.message).fontcolor("red");
             window.document.getElementById("info").appendChild(errorNode);
         });
         console.error = this.fakeError.bind(this);
-
+        
+        // Record whether we're on Safari or Mobile (unused so far)
         this.safari = /(Safari)/g.test( navigator.userAgent ) && ! /(Chrome)/g.test( navigator.userAgent );
-        this.mobile = /(Android|iPad|iPhone|iPod)/g.test( navigator.userAgent ) || this.safari;
-        if (this.mobile) { console.error("Mobile Debugging Enabled"); }
-
-        this.engine.registerCallback("error", this.fakeError.bind(this));
+        this.mobile = /(Android|iPad|iPhone|iPod)/g.test(navigator.userAgent) || this.safari;
     }
 
+    // Log Errors as <div>s over the main viewport
     fakeError(...args) {
         if (args.length > 0) {
             let errorNode = window.document.createElement("div");
