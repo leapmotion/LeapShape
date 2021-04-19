62880d46a855d80bc2ac2e9cb666ac640c4e3080
Johnathon Selstad <makeshifted@gmail.com>
2021-04-19 15:53:58 -0700

    Begin LeapTemporalWarping Implementation

---
+++ src/Frontend/Input/LeapTemporalWarping.js
@@ -0,0 +1,94 @@
+import * as THREE from '../../../node_modules/three/build/three.module.js';
+import "../../../node_modules/leapjs/leap-1.1.0.js";
+import { World } from '../World/World.js';
+
+/** This resamples HMD Transforms to the moment the tracking frame was captured.  
+ * This eliminates swim when the user shakes their head but holds their hands still. */
+class LeapTemporalWarping {
+    /** This resamples HMD Transforms to the moment the tracking frame was captured.  
+     * This eliminates swim when the user shakes their head but holds their hands still.
+     * @param {World} world */
+    constructor(world, interpolator) {
+        this.world = world;
+        this.interpolator = interpolator;
+
+        this.nowToLeapOffsetUS = 0;
+        this.interpolatedTransform = new THREE.Matrix4();
+        this.historyLength = 20;
+        this.history = new window.Leap.CircularBuffer(20);
+
+        this.interpolatedFrame = {
+            timestamp : 0,
+            position  : this.world.camera.position  .clone(),
+            quaternion: this.world.camera.quaternion.clone() 
+        };
+
+        // Temporary Swap Variables
+        this.vec = new THREE.Vector3(); this.vec2 = new THREE.Vector3(); this.vec3 = new THREE.Vector3();
+        this.quat = new THREE.Quaternion(); this.quat2 = new THREE.Quaternion();
+        this.mat1 = new THREE.Matrix4(); this.mat2 = new THREE.Matrix4();
+    }
+
+    /** Adds new element to the history and resamples a current time matrix. */
+    update() {
+        // Add a new Head Transform to the history
+        let currentTimestamp = (this.world.now * 1000) + this.interpolator.nowToLeapOffsetUS;
+        if (this.history.get(this.historyLength - 1)) {
+            let sample = this.history.get(this.historyLength - 1);
+            sample.timestamp = currentTimestamp;
+            sample.position  .copy(this.world.camera.position  );
+            sample.quaternion.copy(this.world.camera.quaternion);
+            this.history.push(sample);
+        } else {
+            this.history.push({
+                timestamp : currentTimestamp,
+                position  : this.world.camera.position  .clone(),
+                quaternion: this.world.camera.quaternion.clone()
+            });
+        }
+
+        return this.getInterpolatedFrame(this.interpolatedFrame, this.history, interpolatedFrameTimestamp-14000);
+    }
+
+    /** Interpolates a frame to the given timestamp 
+     * @param {number} timestamp */
+    getInterpolatedFrame(frame, history, timestamp) {
+        // Step through time until we have the two frames we'd like to interpolate between.
+        let back = 0, doubleBack = 1;
+        let aFrame = history.get(back+doubleBack);
+        let bFrame = history.get(back);
+        while (aFrame.timestamp === bFrame.timestamp && doubleBack < 10) {
+            doubleBack += 1; aFrame = history.get(back + doubleBack);
+        }
+        while (!(bFrame.timestamp < timestamp ||
+                (aFrame.timestamp < timestamp && bFrame.timestamp > timestamp) ||
+                back == 198)) { // Only 200 entries in the history buffer
+            back++;
+            doubleBack = 1;
+            aFrame = history.get(back+doubleBack);
+            bFrame = history.get(back  );
+            while (aFrame.timestamp === bFrame.timestamp && doubleBack < 10) {
+                doubleBack += 1; aFrame = history.get(back + doubleBack);
+            }
+        }
+
+        let aTimestamp = aFrame.timestamp, bTimestamp = bFrame.timestamp;
+        let alpha = (timestamp - aTimestamp) / (bTimestamp - aTimestamp);
+
+        // Debug visualize the temporal offset
+        //this.world.parent.tools.cursor.updateTarget(this.vec.set(0,0,0)); 
+        //this.world.parent.tools.cursor.updateLabel(timestamp - aTimestamp);//this.nowToLeapOffsetUS);
+
+        frame.timestamp = this.lerp           (aTimestamp       , bTimestamp       , alpha);
+        frame.position       . lerpVectors    (aFrame.position  , bFrame.position  , alpha);
+        frame.quaternion     .slerpQuaternions(aFrame.quaternion, bFrame.quaternion, alpha);
+        return frame;
+    }
+
+    /** Linearly Interpolate `a` to `b` by `alpha`
+     * @param {number} a @param {number} b @param {number} alpha @returns {number} */
+    lerp(a, b, alpha) { return (a * (1 - alpha)) + (b * alpha); }
+
+}
+
+export { LeapTemporalWarping };
